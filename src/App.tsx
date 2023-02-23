ca345706896d4a42654e4c06c68965080ddb939f
Renee Paczkowski <rpaczkowski@gmail.com>
2023-02-23 16:44:43 -0500

    Added an AppShell to the page

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,32 @@
+import { AppShell, Header, Navbar } from "@mantine/core";
 import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <h1 className="text-3xl font-bold underline">It Ticket Tracker</h1>
-    </div>
+    <AppShell
+      padding="md"
+      // navbar={
+      //   // <Navbar width={{ base: 300 }} height={500} p="xs">
+      //   //   {/* Navbar content */}
+      //   // </Navbar>
+      // }
+      header={
+        <Header height={60} p="xs">
+          <h1 className="text-3xl font-bold underline">It Ticket Tracker</h1>
+        </Header>
+      }
+      styles={(theme) => ({
+        main: {
+          backgroundColor:
+            theme.colorScheme === "dark"
+              ? theme.colors.dark[8]
+              : theme.colors.gray[0],
+        },
+      })}
+    >
+      {" "}
+      <div className="App"></div>
+    </AppShell>
   );
 }
 
