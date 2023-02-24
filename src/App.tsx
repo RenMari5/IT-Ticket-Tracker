09cbd2a15aac9d102f706e29cd3c157149fed271
Renee Paczkowski <rpaczkowski@gmail.com>
2023-02-24 11:11:01 -0500

    Simplified app by removing Mantine for now

--- src/App.tsx
+++ src/App.tsx
@@ -3,30 +3,9 @@ import "./App.css";
 
 function App() {
   return (
-    <AppShell
-      padding="md"
-      // navbar={
-      //   // <Navbar width={{ base: 300 }} height={500} p="xs">
-      //   //   {/* Navbar content */}
-      //   // </Navbar>
-      // }
-      header={
-        <Header height={60} p="xs">
-          <h1 className="text-3xl font-bold underline">It Ticket Tracker</h1>
-        </Header>
-      }
-      styles={(theme) => ({
-        main: {
-          backgroundColor:
-            theme.colorScheme === "dark"
-              ? theme.colors.dark[8]
-              : theme.colors.gray[0],
-        },
-      })}
-    >
-      {" "}
-      <div className="App"></div>
-    </AppShell>
+    <div className="App">
+      <h1 className="text-3xl font-bold underline">It Ticket Tracker</h1>
+    </div>
   );
 }
 
